11ed63f4cff50970ad56f6e3a91cb0f8108bf5fd
Kosta Eleftheriou <kostas.eleftheriou@gmail.com>
2018-09-03 09:58:27 -0700

    Initial commit

---
+++ AccessibilityKit/AccessibilityKit/AccessibilityKit.h
@@ -0,0 +1,19 @@
+//
+//  AccessibilityKit.h
+//  AccessibilityKit
+//
+//  Created by Kosta Eleftheriou on 9/3/18.
+//  Copyright © 2018 Kpaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AccessibilityKit.
+FOUNDATION_EXPORT double AccessibilityKitVersionNumber;
+
+//! Project version string for AccessibilityKit.
+FOUNDATION_EXPORT const unsigned char AccessibilityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccessibilityKit/PublicHeader.h>
+
+
