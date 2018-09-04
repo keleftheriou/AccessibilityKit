821c27386966b0f1f1e3cf9481b453c5a07ad336
Kosta Eleftheriou <kostas.eleftheriou@gmail.com>
2018-09-03 17:28:22 -0700

    Tidy example

--- AccessibilityKit/AccessibilityKitExamples/ViewController.swift
+++ AccessibilityKit/AccessibilityKitExamples/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UITableViewController {
     "It works 💪",
     "Testing",
     "This is interesting",
-    "Line one...\nLine two!",
+    "Line one.\nLine two!",
     ":)",
     "More text",
     "CAPITAL LETTERS",
@@ -81,12 +81,24 @@ extension ViewController {
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let bgColors = [UIColor.gray, UIColor.brown, UIColor.orange, UIColor.red, UIColor.magenta, UIColor.blue, UIColor.green, UIColor.cyan]
     let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! MaxFontTextViewCell
-    cell.maxFontTextView.attributedText = .init(string: "\(dataSource[indexPath.row])", attributes: [.foregroundColor: UIColor.white.withAlphaComponent(1.0)])
+    
+    // Pretty background colors
+    let bgColors = [UIColor.gray, UIColor.brown, UIColor.orange, UIColor.red, UIColor.magenta, UIColor.blue, UIColor.green, UIColor.cyan]
     cell.gradient.colors = [bgColors[indexPath.row % bgColors.count].cgColor, UIColor.black.cgColor]
     cell.gradient.startPoint = .init(x: -0.4, y: -0.4) // default is (0.5, 0.0)
     cell.gradient.endPoint   = .init(x:  1.4, y:  1.4) // default is (0.5, 1.0)
+    
+    // This paragraph style is the default - putting it here so it's easier to experiment with.
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineBreakMode = .byWordWrapping
+    paragraphStyle.alignment = .center
+    
+    // This vertical alignment is the default - putting it here so it's easier to experiment with.
+    cell.maxFontTextView.verticalAlignment = .center
+    
+    // Set the attributed string that will fit the entire bounds of this cell
+    cell.maxFontTextView.attributedText = .init(string: dataSource[indexPath.row], attributes: [.foregroundColor: UIColor.white, .paragraphStyle: paragraphStyle])
     return cell
   }
   
