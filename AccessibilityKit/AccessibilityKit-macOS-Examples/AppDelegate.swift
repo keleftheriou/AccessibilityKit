f894a87344f5a140848b2d0b86e1c12c0b5f089f
Kosta Eleftheriou <kostas.eleftheriou@gmail.com>
2018-09-16 12:08:58 -0700

    Add skeleton code for macOS example

--- AccessibilityKit/AccessibilityKit-macOS-Examples/AppDelegate.swift
+++ AccessibilityKit/AccessibilityKit-macOS-Examples/AppDelegate.swift
@@ -10,18 +10,37 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
+  
+  @IBOutlet weak var window: NSWindow!
+  
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+    let controller = ViewController()
+    window!.contentView!.addSubview(controller.view)
+    window!.makeKey()
+  }
+  
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+  
 }
 
+class ViewController: NSViewController {
+  
+  override func loadView() {
+    let view = NSView()
+    view.autoresizingMask = [.width, .height]
+    view.wantsLayer = true
+    view.layer?.backgroundColor = NSColor.green.cgColor
+    view.layer?.borderWidth = 2
+    view.layer?.borderColor = NSColor.blue.cgColor
+    self.view = view
+  }
+  
+  override func viewWillLayout() {
+    super.viewWillLayout()
+    view.frame = view.superview!.bounds
+  }
+  
+}
