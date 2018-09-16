393d38cd6efa15b6910d1c7ef2436b2c5fa5fdd9
Kosta Eleftheriou <kostas.eleftheriou@gmail.com>
2018-09-16 11:00:14 -0700

    Add macOS target

---
+++ AccessibilityKit/AccessibilityKit-macOS/AccessibilityKit_macOS.h
@@ -0,0 +1,19 @@
+//
+//  AccessibilityKit_macOS.h
+//  AccessibilityKit-macOS
+//
+//  Created by Kosta Eleftheriou on 9/16/18.
+//  Copyright © 2018 Kpaw. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AccessibilityKit_macOS.
+FOUNDATION_EXPORT double AccessibilityKit_macOSVersionNumber;
+
+//! Project version string for AccessibilityKit_macOS.
+FOUNDATION_EXPORT const unsigned char AccessibilityKit_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccessibilityKit_macOS/PublicHeader.h>
+
+
